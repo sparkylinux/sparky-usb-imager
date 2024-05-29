15fdefc4eeffdfcfdfa91c827414b33e36257218
pavroo <pavroo@onet.eu>
2024-05-29 17:30:56 +0200

    20240529

---
+++ sparky-usb-imager/lang/es_AR.js
@@ -0,0 +1,12 @@
+// English localization file for main
+// Originally created by Piotr "MoroS" Mrożek 2018
+// Under the GNU GPL v3
+// Last update 2024/01/12 by pavroo
+dict.entries['main'] = {};
+// main menu
+dict.entries['main']['formatter0'] = "Formatear un disco USB";
+dict.entries['main']['live0'] = "Crear un USB Linux Vivo";
+dict.entries['main']['persistent0'] = "Crear un disco USB Vivo de SparkyLinux con persistencia";
+dict.entries['main']['formatter1'] = "Borrar un disco USB. Se eliminarán todos los datos de la memoria USB";
+dict.entries['main']['live1'] = "Crear un disco USB arrancable con una Distribución Linux";
+dict.entries['main']['persistent1'] = "Crear un disco USB arrancable con persistencia: Sólo para SparkyLinux";
