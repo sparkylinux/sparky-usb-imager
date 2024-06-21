5e46077c4f688cb9bd901966a1780c83c9b5ea90
pavroo <pavroo@onet.eu>
2024-06-21 14:14:59 +0200

    20240621

---
+++ sparky-usb-imager/lang/hu.js
@@ -0,0 +1,13 @@
+// Hungarian localization file for main
+// Originally created by Piotr "MoroS" Mrożek 2018
+// Under the GNU GPL v3
+// Translated by nemomen
+// Last update 2024/06/21 by pavroo
+dict.entries['main'] = {};
+// main menu
+dict.entries['main']['formatter0'] = "Formázza meg az USB diszket!";
+dict.entries['main']['live0'] = "Hozzon létre live Linux USB-t!";
+dict.entries['main']['persistent0'] = "Hozzon létre SparkyLinux live USB diszket tartósan!";
+dict.entries['main']['formatter1'] = "Az USB diszk megtisztítása. Ez minden adatot el fog távolítani az USB-ről";
+dict.entries['main']['live1'] = "Hozzon létre bootolható USB diszket bármelyik Linux disztribúcióval!";
+dict.entries['main']['persistent1'] = "Hozzon létre bootolható USB diszket tartósan: csak SparkyLinux";
---
+++ sparky-usb-imager/lang/uk.js
@@ -0,0 +1,13 @@
+// Файл української локалізації для Sparky USB Imager
+// Translated by S1LVE12R47 (formerly XalisRatt) [2024/06/07] - matrix : r47s:ilyamikcoder.com
+// Originally created by Piotr "MoroS" Mrożek 2018
+// Under the GNU GPL v3
+// Last update 2024/01/12 by pavroo
+dict.entries['main'] = {};
+// main menu
+dict.entries['main']['formatter0'] = "Форматувати USB-диск";
+dict.entries['main']['live0'] = "Створити Linux Live-CD";
+dict.entries['main']['persistent0'] = "Створити SparkyLinux Live-CD з персистентністю";
+dict.entries['main']['formatter1'] = "Очистити USB-диск. Це видалить всі дані з пристрою USB";
+dict.entries['main']['live1'] = "Створити Live-CD з будь-яким дистрибутивом Linux";
+dict.entries['main']['persistent1'] = "Створити Live-CD з персистентністю: Тільки для SparkyLinux";
