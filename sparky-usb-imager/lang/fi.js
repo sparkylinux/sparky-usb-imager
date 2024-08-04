9f2d49a8b526163c5d44d3954338a239f73616af
pavroo <pavroo@onet.eu>
2024-08-04 13:40:07 +0200

    20240804

---
+++ sparky-usb-imager/lang/fi.js
@@ -0,0 +1,12 @@
+// Suomenkielinen lokalisointitiedosto main
+// Alunperin luonut Piotr "MoroS" MroÅ¼ek 2018
+// GNU GPL v3:n alla
+// Viimeisin päivitys 2024/01/12, pavroo
+dict.entries['main'] = {};
+// päävalikko
+dict.entries['main']['formatter0'] = "Alusta USB-levy";
+dict.entries['main']['live0'] = "Luo live Linux USB";
+dict.entries['main']['persistent0'] = "Luo SparkyLinuxin live-USB-levy pysyvästi";
+dict.entries['main']['formatter1'] = "Puhdista USB-levy. Se poistaa kaikki tiedot USB:stä";
+dict.entries['main']['live1'] = "Luo käynnistettävä USB-levy minkä tahansa Linux-jakelun kanssa";
+dict.entries['main']['persistent1'] = "Luo käynnistettävä USB-levy kestävyydellä: vain SparkyLinux";
