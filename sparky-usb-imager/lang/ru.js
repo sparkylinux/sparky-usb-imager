6a8759747a3e2f8455e31097d0c841b290c25f7f
pavroo <pavroo@onet.eu>
2024-08-29 12:32:57 +0200

    20240829

---
+++ sparky-usb-imager/lang/ru.js
@@ -0,0 +1,12 @@
+// Russian localization file
+// Originally created by Piotr "MoroS" Mrożek 2018
+// Under the GNU GPL v3
+// Last update 2024/01/12 by pavroo
+dict.entries['main'] = {};
+// main menu
+dict.entries['main']['formatter0'] = "Форматирование USB-диска";
+dict.entries['main']['live0'] = "Создание live USB-накопителя Linux";
+dict.entries['main']['persistent0'] = "Создайте live USB-диск SparkyLinux с сохранением";
+dict.entries['main']['formatter1'] = "Очистите USB-диск. Он удалит все данные с USB.";
+dict.entries['main']['live1'] = "Создайте загрузочный USB-диск с любым дистрибутивом Linux.";
+dict.entries['main']['persistent1'] = "Создать загрузочный USB-диск с сохранением: только для SparkyLinux.";
