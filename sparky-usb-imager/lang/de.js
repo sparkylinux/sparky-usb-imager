285d469add7300f38ea2de87e7894a76ed3d0ca8
pavroo <pavroo@onet.eu>
2025-05-30 17:49:03 +0200

    20250530

---
+++ sparky-usb-imager/lang/de.js
@@ -0,0 +1,12 @@
+// German localization file for main (27.05.2025)
+// Originally created by Piotr "MoroS" Mrożek 2018
+// Under the GNU GPL v3
+// Last update 2024/01/12 by pavroo
+dict.entries['main'] = {};
+// main menu
+dict.entries['main']['formatter0'] = "USB-Datenträger formatieren";
+dict.entries['main']['live0'] = "Live-Linux-USB erstellen";
+dict.entries['main']['persistent0'] = "SparkyLinux-Live-USB-Datenträger mit Persistenz erstellen";
+dict.entries['main']['formatter1'] = "USB-Datenträger bereinigen. Es werden alle Daten vom USB entfernt";
+dict.entries['main']['live1'] = "Bootfähigen USB-Datenträger mit einer beliebigen Linux-Distribution erstellen";
+dict.entries['main']['persistent1'] = "Bootfähigen USB-Datenträger mit Persistenz erstellen: Nur SparkyLinux";
